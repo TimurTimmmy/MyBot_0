1a8eed5d830339b12795f8b061c07850dd612ea5
Tim <ash123@mail.ru>
2018-04-22 19:31:50 +0300

    Add keyboard

--- src/Bot.java
+++ src/Bot.java
@@ -1,10 +1,14 @@
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -18,7 +22,7 @@ public class Bot extends TelegramLongPollingBot {
                 sendMsg(msg, "Добро пожаловать!");
                 break;
 
-            case "/pogoda":
+            case "Погода в Москве":
                 try {
                     Weather wt = new Weather();
                     sendMsg(msg, wt.getWeather());
@@ -27,7 +31,7 @@ public class Bot extends TelegramLongPollingBot {
                 }
                 break;
 
-            case "/currency":
+            case "Курс валют":
                 try {
                     Currency cr = new Currency();
                     sendMsg(msg, cr.getCurrency());
@@ -36,7 +40,7 @@ public class Bot extends TelegramLongPollingBot {
                 }
                 break;
 
-            case "/anekdot":
+            case "Случайный анекдот":
                 try {
                     Anekdot an = new Anekdot();
                     sendMsg(msg, an.getAnekdot());
@@ -45,7 +49,7 @@ public class Bot extends TelegramLongPollingBot {
                 }
                 break;
 
-            case "/news":
+            case "Лента новостей":
                 try {
                     News news = new News();
                     sendMsg(msg, news.getNews());
@@ -53,9 +57,9 @@ public class Bot extends TelegramLongPollingBot {
                     e.printStackTrace();
                 }
                 break;
-
+            default:
+                    sendMsg(msg, "Введите команду");
         }
-
     }
 
 
@@ -71,11 +75,43 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     private void sendMsg(Message msg, String text) {
-        SendMessage s = new SendMessage();
-        s.setChatId(String.valueOf(msg.getChatId()));
-        s.setText(text);
+        SendMessage sendMessage = new SendMessage();
+
+        sendMessage.enableMarkdown(true);
+
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboad(false);
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add("Погода в Москве");
+        keyboardFirstRow.add("Курс валют");
+
+        // Вторая строчка клавиатуры
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+        keyboardSecondRow.add("Случайный анекдот");
+        keyboardSecondRow.add("Лента новостей");
+
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        // и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+
+
+        sendMessage.setChatId(String.valueOf(msg.getChatId()));
+        sendMessage.setText(text);
         try {
-            sendMessage(s);
+            sendMessage(sendMessage);
         } catch (TelegramApiException e){
             e.printStackTrace();
         }
